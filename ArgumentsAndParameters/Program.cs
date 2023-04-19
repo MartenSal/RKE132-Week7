fe741e4c88760739a7bde21dd06751d59e7e2c88
Märten <marten.salong@tktk.ee>
2023-04-19 13:48:13 +0300

    add week7 code

---
+++ ArgumentsAndParameters/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Enter a word");
+string userInput = Console.ReadLine();
+
+PrintAnyword(userInput);  //userInput = argument
+
+
+static void PrintAnyword(string anyString) //anyString = parameter
+{
+    anyString = anyString.ToUpper();
+    for(int i = 0; i < 5; i++)
+    {
+        Console.WriteLine(anyString);
+    }
+}
---
+++ Week7Functions/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Are you coming or leaving? (in/out):");
+String userChoice = Console.ReadLine();
+
+if (userChoice == "in")
+{
+    PrintHello();
+}
+else
+{
+    PrintGoodBye();
+
+}
+
+
+static void PrintHello() //funtion / method
+{
+    Console.WriteLine("Hello, World!");
+}
+
+static void PrintGoodBye()
+{
+    Console.WriteLine("See you later, aligator.");
+}
